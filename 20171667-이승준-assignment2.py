f815aaa3deb920fecc192cd2c5c421f83313cc5d
Seungjun Lee <juns519@kookmin.ac.kr>
2017-09-10 00:22:40 +0900

    Revise rescue

--- 20171667-이승준-assignment2.py
+++ 20171667-이승준-assignment2.py
@@ -8,12 +8,12 @@ def isNumber(s):
 
 while True:
     num=input("Enter a number :")#수를 입력받음 
-    if isNumber(num) and int(num)>=-1: #입력받은수가 정수이고 양수인지 판별
-        if int(num)==-1 : # 입력받은 수가 -1이면 프로그램 종료 
+    if isNumber(num) and int(num) >= -1: #입력받은수가 정수이고 양수인지 판별
+        if int(num )== -1 : # 입력받은 수가 -1이면 프로그램 종료 
             break
         else :
             ans = 1
             for i in range(1, int(num)):
                 ans = ans * (i + 1)#입력받은 수가 -1이 아니라면 팩토리얼 계산
-            print(num,end='')
-            print('! =',ans)#결과값 출력
+            print(num, end='')
+            print('! =', ans)#결과값 출력
